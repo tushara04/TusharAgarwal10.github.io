753318ee4779d615d3000736e8447b4751767195
TusharAgarwal10 <tusharagarwal1016@gmail.com>
2024-04-14 02:38:13 +0530

    lots of changes

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Add any interactive elements or functionality here
+function scrollToSection(sectionId) {
+    var targetSection = document.querySelector(sectionId);
+  
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('header');
+    if (window.scrollY > 0) {
+        navbar.classList.add('fixed');
+    } else {
+        navbar.classList.remove('fixed');
+    }
+});
